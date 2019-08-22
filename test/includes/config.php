488a62651b24b72fbf432b9ac5611de979e711aa
kartofun <pavelyg@mail.ru>
2019-08-22 17:46:41 +0300

    test php
    test php

---
+++ test/includes/config.php
@@ -0,0 +1,12 @@
+<?php
+$config = array(
+	'title' => 'Блог туриста',
+	'vk_url' => 'http://vk.com/pavelyg',
+	'db' => array(
+				'server' => 'localhost',
+				'username' => 'root',
+				'password' => '', 
+				'name' => 'test_blog'
+			)
+);
+require "db.php";
